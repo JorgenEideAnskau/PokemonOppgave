85929b0ad9c1b90b5b5f24c79f3c4102a3123a19
JorgenEideAnskau <83663255+JorgenEideAnskau@users.noreply.github.com>
2026-02-20 12:38:58 +0100

    Simplified the README.MD file to not handhold the students, and added a few comments to the CSS and HTML files.

--- pokemonSearch.js
+++ pokemonSearch.js
@@ -1,42 +1,46 @@
-document.getElementById("pokemonName").addEventListener("keydown", (e) => {
-  if (e.key === "Enter") {
+// 1. Trigger on "Enter"
+document.getElementById("pokemonName").addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
     fetchPokemon();
   }
 });
 
+// 2. Created the fetchPokemon() function
 async function fetchPokemon() {
-  // Getting the elements
+  // Grabbed the elements
   const input = document.getElementById("pokemonName");
-  const name = input.value.trim().toLowerCase();
+  const name = input.value.trim();
   const image = document.getElementById("pokemonSprite");
   const error = document.getElementById("errorText");
 
-  //Resetting the UI
+  // 3. Resetting the UI
   input.value = "";
-  image.src = "";
+  image.scr = "";
   image.style.display = "none";
 
-  //Validate Input
+  // 4. Validating input
   if (!name) {
-    error.textContent = "Please enter a Pokémon name";
+    error.textContent = "Please enter a Pokemon name!";
     error.classList.add("text-danger");
     return;
   }
 
-  //Loading state
+  // 6. Loading state
   error.classList.remove("text-danger");
   error.textContent = "Loading ...";
 
-  //Fetching PokéAPI
+  // 5. Fetching and displaying Pokemon
   try {
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
-    if (!response.ok) throw new Error("Pokemon not found 😔");
+    if (!response.ok) throw new Error("Pokemon not found 😔 ");
 
     const data = await response.json();
     image.src = data.sprites.front_default;
     image.style.display = "block";
     error.textContent = "";
+    
   } catch (err) {
+
     error.classList.add("text-danger");
     error.textContent = err.message;
   }
